81a6ed2bb3c645254ce31cf0fbd0dfa82b971b0a
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-12-06 00:48:12 -0600

    remove print statements from Day1.lua

--- Day1.lua
+++ Day1.lua
@@ -1,16 +1,11 @@
 input_text = '((((())))'
 
--- there are several ways to do this; not sure what's quickest
--- 1) have an integer that counts up or down while iterating through each character
--- 2) count all '(' and ')' characers, then find difference. 
--- 2 seems more advanced so let's try it. 
+-- count all '(' and ')' characers, then find difference. 
 
 function count_char(character,input_text)
 	local count = 0
 	for i, string.len(input_text) do
- print('On iteration ' .. i)
 	if input_text[i] == character then count = count + 1
- print(count)
  end
 	end
 	return count
