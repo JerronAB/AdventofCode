4bef58d2ccc166a36235d659a75b345158882c60
JerronAB <jerronaboling@gmail.com>
2023-12-06 08:26:45 -0600

    Completed day 1 first challenge

--- Day1.lua
+++ Day1.lua
@@ -1,12 +1,17 @@
-input_text = '((((())))'
+function lines_from(file)
+  local lines = ''
+  for line in io.lines(file) do lines = lines .. line  end
+  return lines
+end
+
+input_text = lines_from('InputDay1.txt')
 
 -- count all '(' and ')' characers, then find difference. 
 
 function count_char(character,input_text)
 	local count = 0
-	for i, string.len(input_text) do
-	if input_text[i] == character then count = count + 1
- end
+	for i=1,string.len(input_text) do
+	if string.sub(input_text,i,i) == character then count = count + 1 end
 	end
 	return count
 end
