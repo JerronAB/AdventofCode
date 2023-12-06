a12afda7844bccf6d342590d6d1bd424f1828ed9
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-12-05 23:31:23 -0600

    Init via upload

---
+++ Day1.lua
@@ -0,0 +1,10 @@
+input_text = '(((())))'
+
+-- there are several ways to do this; not sure what's quickest
+-- 1) have an integer that counts up or down while iterating through each character
+-- 2) count all '(' and ')' characers, subtract. 
+-- 2 seems more advanced so let's try it. 
+
+function count_char(character,input_text)
+    --body
+end
