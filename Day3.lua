b069d195bf1517539ac546908fe220c11590ab24
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-12-07 08:34:35 -0600

    day 3 partial

---
+++ Day3.lua
@@ -0,0 +1,28 @@
+file = io.open('InputDay3.txt', 'r'); file_content = file:read "*all"; file:close()
+
+xcoord, ycoord = 0, 0
+visited = {}; visited[0] = {}; visited[0][0] = 0
+-- visited = {0 = {0 = true}} --indexes are the coordinates
+function move(input)
+	if     input == '>' then xcoord = xcoord + 1
+	elseif input == '^' then ycoord = ycoord + 1
+	elseif input == '<' then xcoord = xcoord - 1
+	elseif input == 'v' then ycoord = ycoord - 1
+	end
+	if not visited[xcoord] then visited[xcoord] = {} end
+	visited[xcoord][ycoord] = true
+end
+
+for i=1, #file_content do move(string.sub(file_content, i, i)) end--#file_content is equivalent to string.len(file_content)
+	--[[for _xcoord,_ycoords in pairs(visited) do 
+	for _ycoord,_ in pairs(_ycoords) do
+	print('x-coord: ' .. _xcoord .. '  y-coord: ' .. _ycoord) 
+	end 
+	end]]
+
+i = 0
+for _x,_ycoords in pairs(visited) do
+	for _ycoord,_ in pairs(_ycoords) do
+	i = i + 1 end end
+
+print('Santa has visited ' .. i .. ' houses.') --represents number of houses (coords) hit. 
