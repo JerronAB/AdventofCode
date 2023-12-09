2323c12066ff33bd93b38f725dab0b74985ee57b
JerronAB <jerronaboling@gmail.com>
2023-12-09 09:52:01 -0600

    incremental changes to lua day 5

--- Day5.lua
+++ Day5.lua
@@ -1,4 +1,8 @@
 goodLines = 0
+function add(item)
+	return item + 1
+end
+
 for line in io.lines('InputDay5.txt') do
 	if not string.find(line, '[ab][cd][pq][xy]') then 
 	local i = 0
@@ -11,11 +15,15 @@ for line in io.lines('InputDay5.txt') do
 		i = i + 1
 		char = string.sub(line, i, i)
 		if vs[char] then vowelCount = vowelCount + 1 end
-		if letters[char] then letters[char] = letters[char] + 1 else letters[char] = 1 end --is there a slightly better way here?
+		-- if letters[char] then letters[char] = letters[char] + 1 else letters[char] = 1 end --is there a slightly better way here?
+		_,value = pcall(add, letters[char])
+		letters[char] = (value or _) or 1
 		duplicate = (char == duplicate) or duplicate --will always return true if it's already been true once
 		goodString = duplicate and vowelCount > 2
 	until goodString
-	if goodString then print(line); goodLines = goodLines + 1 end
+	if goodString then print('This string is good: ' .. line); goodLines = goodLines + 1 
+	else print('This string is bad: ' .. line)
+	end
 	end
 end
 
