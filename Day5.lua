5a5b42b716c9f55994a4e5ed6f0feb693021f09c
JerronAB <jerronaboling@gmail.com>
2023-12-09 01:38:40 -0600

    inc changes to day 5

--- Day5.lua
+++ Day5.lua
@@ -1,22 +1,21 @@
 goodLines = 0
 for line in io.lines('InputDay5.txt') do
-	start,finish = string.find(line, '[aeiou]')
+	if not string.find(line, '[ab][cd][pq][xy]') then 
 	local i = 0
 	local letters = {}
 	local goodString = false
 	local duplicate	= false
 	local vowelCount = 0
-	local noBadStrings = true
+	vs = {['a']=true;['e']=true;['i']=true;['o']=true;['u']=true;}
 	repeat 
 		i = i + 1
 		char = string.sub(line, i, i)
-		if {['a']=true;['e']=true;['i']=true;['o']=true;['u']=true;}[char] then vowelCount = vowelCount + 1 end
-		letters[char] = letters[char] + 1 or 1 --this SHOULD nil/error on the first try if that character is unused, and place 1 there when that happens
+		if vs[char] then vowelCount = vowelCount + 1 end
+		if letters[char] then letters[char] = letters[char] + 1 else letters[char] = 1 end --is there a slightly better way here?
 		duplicate = (char == duplicate) or duplicate --will always return true if it's already been true once
-		goodString = duplicate and _3letters and noBadStrings
+		goodString = duplicate and vowelCount > 2
 	until goodString
 	if goodString then print(line); goodLines = goodLines + 1 end
+	end
 end
 
--- duplicates (2 in row), and 3 vowels
-not = {"ab","cd","pq","xy"}
