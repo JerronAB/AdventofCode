f1d340ae01e8d298988083046e6770e5373e987f
JerronAB <jerronaboling@gmail.com>
2023-12-09 11:19:50 -0600

    Small changes to Day5 - incomplete

--- Day5.lua
+++ Day5.lua
@@ -1,29 +1,32 @@
 goodLines = 0
-function add(item)
-	return item + 1
-end
+function add(item) return item + 1 end
 
 for line in io.lines('InputDay5.txt') do
-	if not string.find(line, '[ab][cd][pq][xy]') then 
+	print(line)
 	local i = 0
 	local letters = {}
 	local goodString = false
 	local duplicate	= false
 	local vowelCount = 0
+	if not string.find(line,'ab') or string.find(line,'cd')  or string.find(line,'pq') or string.find(line,'xy') then 
 	vs = {['a']=true;['e']=true;['i']=true;['o']=true;['u']=true;}
 	repeat 
 		i = i + 1
 		char = string.sub(line, i, i)
 		if vs[char] then vowelCount = vowelCount + 1 end
-		-- if letters[char] then letters[char] = letters[char] + 1 else letters[char] = 1 end --is there a slightly better way here?
 		_,value = pcall(add, letters[char])
-		letters[char] = (value or _) or 1
-		duplicate = (char == duplicate) or duplicate --will always return true if it's already been true once
+		letters[char] = (_ and value) or 1 --this will test if add was successful; if so, return value
+		duplicate = duplicate or (char == duplicate)  --will always return true if it's already been true once
 		goodString = duplicate and vowelCount > 2
-	until goodString
+	until goodString or i == #line
+	end
+	if duplicate then local db = 'true' else db = 'false' end
+	print('Duplicate matched: ' .. db)
+	print('Vowelcount: ' .. vowelCount)
+	print('BadStrings: ')
+	if string.find(line,'ab') or string.find(line,'cd')  or string.find(line,'pq') or string.find(line,'xy') then print('true') else print('false') end
 	if goodString then print('This string is good: ' .. line); goodLines = goodLines + 1 
 	else print('This string is bad: ' .. line)
 	end
-	end
 end
-
+print('Number of \'good lines\': ' .. goodLines)
