b15bd454bb855a7f3617b9e8b8f91f2d5914883b
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-12-08 22:36:47 -0600

    Update Day5.lua

--- Day5.lua
+++ Day5.lua
@@ -1,4 +1,15 @@
-for line in io.lines('InputDay5.txt') do --here's my overcomplicated plan:
-				         --I want a metatable, with values for vowels and return nil for non-vowels (not in the table) 
-start,finish = string.find(line, '[aeiou]')
+function str_to_table(str)
+--continue reading about metamethods; I want an efficient method by which we can iterate over strs
 end
+
+for line in io.lines('InputDay5.txt') do
+start,finish = string.find(line, '[aeiou]')
+local letters = {}
+for i=1,#line do 
+char = string.sub(line, i, i)
+letters[char] = 1 or char + 1 --fancy operation that should count up only if it already exists (once I fix it)
+end 
+end
+
+require = {"duplicates (2 in row), and 3 vowels"}
+not = {"ab","cd","pq","xy"}
