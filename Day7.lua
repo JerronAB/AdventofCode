0af60e765320ec0520e4768b0867da1a6cabfc28
JerronAB <jerronaboling@gmail.com>
2023-12-12 18:41:54 -0600

    incremental work on day 7

--- Day7.lua
+++ Day7.lua
@@ -1,14 +1,19 @@
 function ConvertToBinaryList(int)
 	assert(type(int) == "number",'A number must be passed to the convertToBinaryList function. ')
 	local digitList = {}
+	setmetatable(digitList, binaryOnly)
 	local result = int
 	local remainder
+	print('Starting with integer: ' .. int)
 	while result ~= 0 do
-		result = (result / 2) - (result / 2) % 1
-		--print('Dividing previous result by 2, and rounding: ' .. result)
+		if result == 1 then remainder = 1; result = 0
+		else 
 		remainder = result % 2
-		--print('Remainder of that operation: ' .. remainder)
-		table.insert(digitList,remainder) --should insert from MSB to LSB
+		result = (result / 2) - (result / 2) % 1
+		end
+		print('Dividing previous result by 2, and rounding: ' .. result)
+		print('Remainder of that operation: ' .. remainder)
+		table.insert(digitList,1,remainder) --should insert from MSB to LSB
 	end
 	local prntStr = ''
 	for index,_1_or_0_ in ipairs(digitList) do prntStr = prntStr .. _1_or_0_ .. ' ' end
@@ -16,7 +21,33 @@ function ConvertToBinaryList(int)
 	return digitList
 end
 
+BinaryOnly = {
+	__index = function(table, key) -- rawget bypasses metatables to get 'real value'
+		if rawget(table,key) == nil then return 0 else return table[key] end
+	end;
+	__newindex = function(table, key) --ensures these lists are just 1/0's
+		assert(key == 1 or key == 0, "Non-binary key inserted into list.")
+		return table[key]
+	end
+	}
+
+function compareAnd(list1,list2)
+	for i=1,#list1
+end
+
+function compareOr(list1,list2)
+end
+
+function LSHIFT(list)
+end
+
+function RSHIFT(list)
+end
+
+function makeNOT(list)
+end
+
 ConvertToBinaryList(3)
 ConvertToBinaryList(8)
-ConvertToBinaryList(50)
-ConvertToBinaryList(101)
+--ConvertToBinaryList(50)
+--ConvertToBinaryList(101)
