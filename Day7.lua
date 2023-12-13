634285734c2ec6e91413b6e90b0a6c397326e29f
JerronAB <92005206+JerronAB@users.noreply.github.com>
2023-12-13 10:23:55 -0600

    Update Day7.lua

--- Day7.lua
+++ Day7.lua
@@ -1,6 +1,11 @@
+--NOTE: this is broken in part because (I believe) my metatable prevents iteration; it either needs an __iter method or another approach entirely
+
 BinaryOnly = {
 	__index = function(table, key) -- rawget bypasses metatables to get 'real value'
-		if rawget(table,key) == nil then print('Table indexed at a nil index! returning 0 instead'); return 0 else return table[key] end
+		if rawget(table,key) == nil then 
+			--print('Table indexed at a nil index! returning 0 instead'); 
+			return 0 
+		else return table[key] end
 	end;
 	__newindex = function(table, key) --ensures these lists are just 1/0's
 		assert(key == 1 or key == 0, "Non-binary key inserted into list.")
@@ -10,29 +15,40 @@ BinaryOnly = {
 
 
 function ConvBinaryList(int)
+	print('Running ConvBinaryList for integer ' .. int)
 	assert(type(int) == "number",'A number must be passed to the convertToBinaryList function. ')
 	local digitList = {}
-	--setmetatable(digitList, BinaryOnly)
+	setmetatable(digitList, BinaryOnly)
 	local result = int
 	local remainder
-	--print('Starting with integer: ' .. int)
 	while result ~= 0 do
 		if result == 1 then remainder = 1; result = 0
-		else 
+		else
 		remainder = result % 2
 		result = (result / 2) - (result / 2) % 1
 		end
-		--print('Dividing previous result by 2, and rounding: ' .. result)
-		--print('Remainder of that operation: ' .. remainder)
+		print('Dividing previous result by 2, and rounding: ' .. result)
+		print('Remainder of that operation: ' .. remainder)
 		table.insert(digitList,remainder) --left to right: LSB to MSB
 	end
-	--local prntStr = ''
-	--for index,_1_or_0_ in ipairs(digitList) do prntStr = prntStr .. _1_or_0_ .. ' ' end
-	--print(prntStr)
+	print(#digitList)
+	local prntStr = '{'
+	for index,_1_or_0_ in pairs(digitList) do 
+		print('iter')
+		prntStr = prntStr .. _1_or_0_ .. ', ' 
+	end
+	prntStr = prntStr .. '}'
+	print(prntStr)
 	return digitList
 end
 
 function Compare(operator,list1,list2)
+	print('Running ConvBinaryList for these lists:')
+	local prntStr = '{'
+	for index,_1_or_0_ in pairs(list1) do prntStr = prntStr .. _1_or_0_ .. ' ' end
+	for index,_1_or_0_ in pairs(list2) do prntStr = prntStr .. _1_or_0_ .. ' ' end
+	prntStr = prntStr .. '}'
+	print(prntStr)
 	assert(string.upper(operator) == 'OR' or operator == 'AND', "Compare must have parameter 'AND' or 'OR'; got " .. operator)
 	local operation
 	if string.upper(operator) == 'AND' then operation = function (_l1,_l2) 
@@ -44,9 +60,6 @@ function Compare(operator,list1,list2)
 	setmetatable(newBinaryList,BinaryOnly)
 	print('Larger of the two lists is size: ' .. math.max(#list1,#list2)) 
 	for i=1,math.max(#list1,#list2) do print('got to new list gen');table.insert(newBinaryList, operation(list1[i],list2[i])) end
-	local prntStr = ''
-	for index,_1_or_0_ in ipairs(newBinaryList) do prntStr = prntStr .. _1_or_0_ .. ' ' end
-	print(prntStr)
 	return newBinaryList
 end
 
@@ -56,9 +69,10 @@ end
 function RSHIFT(list)
 end
 
-function makeNOT(list)
+function Complement(list)
 end
 
 ConvBinaryList(3)
 ConvBinaryList(8)
+print('---NOW COMPARING---')
 Compare('AND',ConvBinaryList(3),ConvBinaryList(8)) --> should be 1101
