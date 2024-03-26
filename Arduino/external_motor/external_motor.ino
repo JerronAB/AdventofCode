8db3e289056f269e429de87a7d9ae7fcc1091bfe
JerronAB <92005206+JerronAB@users.noreply.github.com>
2024-03-25 20:27:05 -0500

    incremental changes to robot control progs

--- Arduino/external_motor/external_motor.ino
+++ Arduino/external_motor/external_motor.ino
@@ -23,13 +23,17 @@ void loop() { //loop is equivalent to main() in most c programs; runs in forever
  int sensorPin = 8; //this is the pin we initialize for input
  sensorInput = digitalRead(sensorPin); //digitalRead gets the sensor value; in this line the value is assigned to the sensorInput variable
  Serial.print("Sensor input: ");
- Serial.print(sensorInput); //these last 2 lines just display the sensor's output */
+ Serial.print(sensorInput); //these last 2 lines just display the sensor's output
  if (sensorInput == 0) { //testing sensor input
-  Serial.println(" Turning Counter-Clockwise.");
-  newServo.write(90); //turns counter-clockwise
+  Serial.print(" Turning Counter-Clockwise.");
+  newServo.write(180); //turns counter-clockwise
  }
  else if (sensorInput == 1)  {
-  Serial.println(" Turning Clockwise.");
+  Serial.print(" Turning Clockwise.");
   newServo.write(0); //turns clockwise
  }
+ int servoPosition = newServo.read();
+ Serial.print(" - Servo Position: ");
+ Serial.print(servoPosition);
+ Serial.print("\n");
 }
--- Arduino/robot_control/robot_control.ino
+++ Arduino/robot_control/robot_control.ino
@@ -16,20 +16,19 @@ If A == 0 and B == 0, go forward
 Elseif A==1 and B==0, go left
 */
 
-/*Functionalize left, right and forward?*/
-
 #include <Servo.h> //include the servo library
 Servo leftServo; //initialize a servo object
 Servo rightServo;
+int leftSensorPin = 2; 
+int rightSensorPin = 13; 
 
 void setup() { //setup runs once
   Serial.begin(9600); // starts serial comms; sets baud rate to 9600bps
   leftServo.attach(10);
   rightServo.attach(9);
-  pinMode(8, INPUT); // init pin 8 as an input
-  pinMode(11, INPUT); //init pin 11 as input
+  pinMode(leftSensorPin, INPUT); // init pin 8 as an input
+  pinMode(rightSensorPin, INPUT); //init pin 11 as input
   Serial.println("Setup complete.");
-  
 }
 
  void leftTurn() {
@@ -37,39 +36,29 @@ void setup() { //setup runs once
   leftServo.write(0);
  }
  void rightTurn() {
-  rightServo.write(90);
-  leftServo.write(90);
+  rightServo.write(180);
+  leftServo.write(180);
  }
  void straightForward() {
   rightServo.write(0);
-  leftServo.write(90);
+  leftServo.write(180);
  }
 
 void loop() { //loop is equivalent to main() in most c programs; runs in forever 
  //initialize function? 
- bool debug = false
+ bool debug = false;
  int leftSensorInput; //this will be the RESULT of digitalRead
  int rightSensorInput;
- int leftSensorPin = 8; 
- int rightSensorPin = 11; 
  leftSensorInput = digitalRead(leftSensorPin); //digitalRead gets the sensor value
  rightSensorInput = digitalRead(rightSensorPin);
  Serial.print("Left sensor input: ");
  Serial.print(leftSensorInput); //these last 2 lines just display the sensor's output */
  Serial.print(" -- Right sensor input: ");
  Serial.print(rightSensorInput);
- if (leftSensorInput == 0 && rightSensorInput == 0) { //testing sensor input
-  straightForward()
- }
- else if (leftSensorInput == 0 && rightSensorInput == 1)  {
-  rightTurn()
- }
- else if (leftSensorInput == 1 && rightSensorInput == 0)  {
- }
-  leftTurn()
- else if (leftSensorInput == 1 && rightSensorInput == 1)  {
-  straightForward()
- }
+ if (leftSensorInput == 0 && rightSensorInput == 0) {straightForward();}
+ else if (leftSensorInput == 0 && rightSensorInput == 1) {rightTurn();}
+ else if (leftSensorInput == 1 && rightSensorInput == 0)  {leftTurn();}
+ else if (leftSensorInput == 1 && rightSensorInput == 1)  {straightForward();}
 }
-// leftServo.write(90); //turns counter-clockwise
-// rightServo.write(0); //turns clockwise
+//.write(180); //turns counter-clockwise
+//.write(0); //turns clockwise
